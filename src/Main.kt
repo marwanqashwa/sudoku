7b3c666a64e4ef4642312478db5417095a31f203
Marwan Qashwa <marawankashwa24@gmail.com>
2025-04-03 19:53:21 +0200

    task complete

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,6 @@
 
 fun main() {
 
+    Test.checks()
 }
 
-fun sudokuChecker(sudoku: Array<Array<Char>>):Boolean{
-    val sudokuToCheck=separateEachRowColumnSubgrid(sudoku)
-    sudokuToCheck.forEach {
-       if (!checkRowColumnSubgrid(it))return false
-    }
-    return true;
-}
-
-fun separateEachRowColumnSubgrid(arrayBeforeSeparate: Array<Array<Char>>): Array<Array<Char>>{
- val separatedSudoku = mutableListOf<Array<Char>>()
-
-
-
-    return separatedSudoku.toTypedArray()
-}
-
-fun checkRowColumnSubgrid(row:Array<Char>):Boolean{
-
-    return false
-}
