82a7540c3a2fbcd7e29f594183527b92de622fd8
Marwan Qashwa <marawankashwa24@gmail.com>
2025-04-03 17:21:09 +0200

    init

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,3 @@
-/*
-- - - | - - - | - - -
-- - - | - - - | - - -
-- - - | - - - | - - -
----------------------
-- - - | - - - | - - -
-- - - | - - - | - - -
-- - - | - - - | - - -
----------------------
-- - - | - - - | - - -
-- - - | - - - | - - -
-- - - | - - - | - - -
-
-invalid row or column --> - 3 - | 7 - 6 | - 3 -   (There is repetition)
-
-valid row or column   --> - 2 1 | 7 - 6 | - 3 -   (There is no repetition)
-
-invalid 3*3 box       --> 5 - 4
-                          - 6 -  (There is repetition)
-                          7 5 -
-
-valid 3*3 box         --> 6 - -
-                          - 7 4  (There is no repetition)
-                          5 9 -
-*/
 
 fun main() {
 
@@ -37,16 +12,14 @@ fun sudokuChecker(sudoku: Array<Array<Char>>):Boolean{
 }
 
 fun separateEachRowColumnSubgrid(arrayBeforeSeparate: Array<Array<Char>>): Array<Array<Char>>{
-    return arrayOf()
+ val separatedSudoku = mutableListOf<Array<Char>>()
+
+
+
+    return separatedSudoku.toTypedArray()
 }
 
 fun checkRowColumnSubgrid(row:Array<Char>):Boolean{
-    /*
- checkRowColumnSubgrid({- , - , - , - , - , - , - , - , -})--> true
- checkRowColumnSubgrid({- , 4 , 3 , - , 6 , - , - , - , -})--> true
- checkRowColumnSubgrid({1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9})--> true
- checkRowColumnSubgrid({1 , - , 6 , - , 4 , 1 , - , 2 , -})--> false
- checkRowColumnSubgrid({1 , - , 6 , - , 4 , 1 , - , 1 , -})--> false
-    */
+
     return false
 }
